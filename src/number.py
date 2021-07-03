6f79230f00519165178f00ef7d0112c7508a5665
kgw0t <kgw0tsyk@gmail.com>
2021-07-04 02:03:39 +0900

    update number

--- src/number.py
+++ src/number.py
@@ -90,6 +90,102 @@ def radix():
     print(hex)
     print(Hex)
 
+def bool_to_int():
+    '''
+    bool型をint型に変換
+    True は 1
+    False は 0
+    '''
+    n = int(True)
+    print(n)
+
+    n = int(False)
+    print(n)
+
+def float_to_int():
+    '''
+    float型をint型に変換
+    小数点以下は切り捨てられる
+    '''
+    n = int(98.6)
+    print(n)
+
+    n = int(1.0e4)
+    print(n)
+
+def str_to_int():
+    '''
+    str型をint型に変換
+    '''
+    n = '99'
+    print(n)
+
+    n = '-23'
+    print(n)
+
+    n = '+12'
+    print(n)
+
+    # ValueError
+    # n = int('98.6')
+    # n = int('1.0e4')
+
+def auto_cast():
+    '''
+    演算時の自動的な型変換
+    '''
+    n = 4 + 7.0
+    print(n)
+    print(type(n))
+
+    n = True + 2
+    print(n)
+    print(type(n))
+
+    n = False + 5.0
+    print(n)
+    print(type(n))
+
+def googol():
+    '''
+    10 ** 100のような大きい数
+    オーバーフローせずに計算できる
+    '''
+    n = 10 ** 100
+    print(n)
+
+def bool_to_float():
+    '''
+    bool型をfloat型に変換
+    '''
+    n = float(True)
+    print(n)
+
+    n = float(False)
+    print(n)
+
+def int_to_float():
+    '''
+    int型をfloat型に変換
+    '''
+    n = float(98)
+    print(n)
+
+def str_to_float():
+    '''
+    str型をfloat型に変換
+    '''
+    n = float('98')
+    print(n)
+
+    n = float('98.6')
+    print(n)
+
+    n = float('-1.5')
+    print(n)
+
+    n = float('1.0e4')
+    print(n)
 
 if __name__ == '__main__':
     add()
@@ -101,3 +197,11 @@ if __name__ == '__main__':
     operator_with_assign()
     priority()
     radix()
+    bool_to_int()
+    float_to_int()
+    str_to_int()
+    auto_cast()
+    googol()
+    bool_to_float()
+    int_to_float()
+    str_to_float()
